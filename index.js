f916c3c77f6a9e2735c9985674185d93f4a8f228
Natalia De Jager <132550055+NataliaDeJager@users.noreply.github.com>
2023-10-24 16:27:28 +0200

    Create function for Computer Choice
    Assigned number values to Rock Paper Scissors values

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ let randomNumber = (Math.floor(Math.random() * 3) + 1);
 
 function getComputerChoice(){
     if (randomNumber === 1){
-        
+        console.log("Rock");
     }
-
+    else if (randomNumber === 2) {
+        console.log("Paper");
+    }
+    else {
+        console.log("Scissors");
+    }
 };
+
+getComputerChoice();
