1792e966299f8ce3e98ee6e6eeef7fa670e8a3d1
Natalia De Jager <132550055+NataliaDeJager@users.noreply.github.com>
2023-10-24 23:03:01 +0200

    Move randomNmbr create game function modify round
    Moved randomNumber inside getComputerChoice function so it randomizes with each round
    Modified playRound return to return an array with a string and a value
    Created game function with a for loop to loop 5 rounds and give a final result based on scores

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
-let randomNumber = (Math.floor(Math.random() * 3) + 1);
-// Selects random number from 1 2 or 3 
-
 function getComputerChoice(){
+    let randomNumber = (Math.floor(Math.random() * 3) + 1); // Selects random number from 1 2 or 3 
+
     if (randomNumber === 1){
         return "Rock";
     }
@@ -17,41 +16,71 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
 
-    if (computerSelection === "Rock" && playerSelection === "scissors" || computerSelection === "Paper" && playerSelection === "rock" || computerSelection === "Scissors" && playerSelection === "paper") {
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    if (computerSelection === "Rock" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Paper") {
+        return ["You Lose! " + computerSelection + " beats " + playerSelection, 0]; 
     }
-    else if (computerSelection === "Rock" && playerSelection === "paper" || computerSelection === "Paper" && playerSelection === "scissors" || computerSelection === "Scissors" && playerSelection === "rock") {
-        return "You Win! " + playerSelection + " beats " + computerSelection;
+    else if (computerSelection === "Rock" && playerSelection === "Paper" || computerSelection === "Paper" && playerSelection === "Scissors" || computerSelection === "Scissors" && playerSelection === "Rock") {
+        return ["You Win! " + playerSelection + " beats " + computerSelection, 1];
     }
     else {
-        return "Its a tie!";
+        return ["Its a tie!", 2];
     }  
 };
 // Plays one round of the game against computer
+// returns arrays to obtain two different values
 
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
 
-let computerSelection = getComputerChoice();
+    for (var i = 0; i < 5; i++){
 
-let playerSelection = prompt("Please enter your choice");
+        let computerSelection = getComputerChoice();
 
-if (playerSelection !== null) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-     
-    if (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
-        alert("Invalid Input!");
-    } else {
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-  } else { 
-        alert("You didn't make a selection");
-  };
+        let playerSelection = prompt("Please enter your choice");
 
+        if (playerSelection !== null) {
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+            
+            if (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
+                alert("Invalid Input!");
+
+            } else {
+                console.log(computerSelection);
+                let result = playRound(playerSelection, computerSelection);
+                console.log(result[0]);
+
+                if (result[1] === 0){
+                    computerScore++;
+
+                } else if (result[1] === 1){
+                    playerScore++;
+                };
+            }
+
+        } else { 
+                alert("You didn't make a selection");
+        };
+            }
 // Makes sure the input is not empty
 // Converts input into lower case
 // checks whether input is different from rock paper or scissors provides error alert
 // if it is rock paper or scissors begins game.
 // If the input is empty provides error alert.
+// stores results in result variable 
+// adds the results of each round to computer and player scores
+
+    if (computerScore < playerScore){
+        alert("You Win!");
+    } else if (computerScore > playerScore){
+        alert("You Lose!");
+    } else {
+        alert("It's a Tie!")
+    }
+}
+// checks who wins the match by comparing scores
+game();
+
 
 
 
