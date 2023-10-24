d35364f2ea211d7661b3ffbda111fe4aedeb5cb1
Natalia De Jager <132550055+NataliaDeJager@users.noreply.github.com>
2023-10-24 21:54:19 +0200

    Fix code for playerSelection & playRound function
    Moved the playRound function upwards so it doesnt proc after the playerSelection
    playRound function ONLY procs if playerSelection is correctly made.
    playerSelection is now case-insensitive and has first character with uppercase

--- index.js
+++ index.js
@@ -15,22 +15,46 @@ function getComputerChoice(){
 };
 // Assigns the number selected to one of the choices either Rock Paper or Scissors for the computer choice. 
 
-let computerSelection = getComputerChoice();
-
-let playerSelection = prompt("Please enter your choice").toLowerCase();
-
 function playRound(playerSelection, computerSelection) {
 
     if (computerSelection === "Rock" && playerSelection === "scissors" || computerSelection === "Paper" && playerSelection === "rock" || computerSelection === "Scissors" && playerSelection === "paper") {
-        return "You Lose! " + computerSelection + "beats " + playerSelection;
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
     else if (computerSelection === "Rock" && playerSelection === "paper" || computerSelection === "Paper" && playerSelection === "scissors" || computerSelection === "Scissors" && playerSelection === "rock") {
-        return "You Win! " + playerSelection + "beats " + computerSelection;
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     }
     else {
         return "Its a tie!";
-    }
-    
+    }  
 };
+// Plays one round of the game against computer
 
-// There is a problem with the playRound function where its returning a tie for all the games 
+
+let computerSelection = getComputerChoice();
+
+let playerSelection = prompt("Please enter your choice");
+
+if (playerSelection !== null) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+     
+    if (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
+        alert("Invalid Input!");
+    } else {
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+  } else { 
+        alert("You didn't make a selection");
+  };
+
+// Makes sure the input is not empty
+// Converts input into lower case
+// checks whether input is different from rock paper or scissors provides error alert
+// if it is rock paper or scissors begins game.
+// If the input is empty provides error alert.
+
+
+
+
+
+
